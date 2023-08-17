04716d750a8a6add1850b95be097b0ac90986407
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-08-17 14:12:40 +0530

    Ecommerce

--- Backend/Models/userModel.js
+++ Backend/Models/userModel.js
@@ -20,6 +20,10 @@ const userSchema = new Schema(
       type: String,
       require: true,
     },
+    question: {
+      trype: String,
+      require: true,
+    },
     address: {
       type: String,
       require: true,
