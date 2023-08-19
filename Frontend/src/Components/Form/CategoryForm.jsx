16b78c6fcc00acf46dfebf13120b99712abe82b3
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-08-19 22:01:33 +0530

    Ecommerce

--- Frontend/src/Components/Form/CategoryForm.jsx
+++ Frontend/src/Components/Form/CategoryForm.jsx
@@ -1,7 +1,18 @@
 function CategoryForm({handleSubmit, value, setValue}) {
     return (
       <>
-       
+        <form onSubmit={handleSubmit} >
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter New Category"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+         
+        </form>
       </>
     );
   }
