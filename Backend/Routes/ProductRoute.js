27f14d246c945d210eae13654f382f463d12d7e0
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-08-27 20:32:48 +0530

    Ecommerce

--- Backend/Routes/ProductRoute.js
+++ Backend/Routes/ProductRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createProductController, getAllProductsController, getSingleProductsController } from "../Controllers/ProductController.js";
+import {
+  createProductController,
+  getAllProductsController,
+  getSingleProductsController,
+  getProductPhotoContorller,
+} from "../Controllers/ProductController.js";
 import { isAdmin, requireSignIn } from "../Middlewares/authMiddleware.js";
 
 import formidable from "express-formidable";
@@ -13,8 +18,12 @@ router.post(
   formidable(),
   createProductController
 );
-router.get("/get-products", getAllProductsController)
-router.get("/get-products/:slug", getSingleProductsController)
-// router.get("/")
+router.get("/get-products", getAllProductsController);
+router.get("/get-products/:slug", getSingleProductsController);
+
+// get Photo
+router.get("/product-photo/:id", getProductPhotoContorller);
+// router.get("/update-product", updateProductController)
+// router.get("/delete-product", deleteProductController)
 
 export default router;
