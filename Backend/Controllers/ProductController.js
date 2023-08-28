47ad9ad59cc04598eef54cb320622598d89c3c05
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-08-28 14:42:32 +0530

    Ecommerce

--- Backend/Controllers/ProductController.js
+++ Backend/Controllers/ProductController.js
@@ -119,3 +119,51 @@ export const deleteProductController = async (req, res) => {
       .send({ success: false, message: "Error in Delete Product" });
   }
 };
+// Update Product
+export const updateProductController = async (req, res) => {
+  try {
+    // const updateProduct = await productModal.findByIdAndUpdate({});
+
+    const { name, slug, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+    // Validation -- Switch Case
+    switch (true) {
+      case !name:
+        return res.status(500).send({ message: "Name is Required" });
+      case !description:
+        return res.status(500).send({ message: "Description is Required" });
+      case !price:
+        return res.status(500).send({ message: "Price is Required" });
+      case !category:
+        return res.status(500).send({ message: "Category is Required" });
+      case !quantity:
+        return res.status(500).send({ message: "Quantity is Required" });
+      case !photo && photo.size > 1000000:
+        return res
+          .status(500)
+          .send({ message: "Photo is Required and Should be Less Then 1MB" });
+    }
+    //
+    const products = await productModal.findByIdAndUpdate(
+      req.params.id,
+      { ...req.fields, slug: slugify(name) },
+      { new: true }
+    );
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
+    }
+    await products.save();
+    res.status(201).send({
+      success: true,
+      message: "Product Updated Successfull",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ success: false, message: "Error In Update Product" });
+  }
+};
