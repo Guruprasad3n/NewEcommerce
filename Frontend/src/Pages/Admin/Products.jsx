850b3d3d8b80fa13d43ac43f0bc7c1f3063383a5
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-08-31 23:18:17 +0530

    Ecommerce

--- Frontend/src/Pages/Admin/Products.jsx
+++ Frontend/src/Pages/Admin/Products.jsx
@@ -1,4 +1,27 @@
 import React, { useEffect, useState } from "react";
+import Layout from "../../Components/Layout/Layout";
+import AdminMenu from "../../Components/Layout/AdminMenu";
+import axios from "axios";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Stack,
+  StackDivider,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+
+
 
 
 function Products() {
