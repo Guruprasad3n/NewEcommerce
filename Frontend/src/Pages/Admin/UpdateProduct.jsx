6f1cd3ad4e80486274682b5dab88ccd923f9de47
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-09-04 14:34:55 +0530

    Ecommerce

--- Frontend/src/Pages/Admin/UpdateProduct.jsx
+++ Frontend/src/Pages/Admin/UpdateProduct.jsx
@@ -1,10 +1,119 @@
+import { Button, Select } from "@chakra-ui/react";
 import Layout from "../../Components/Layout/Layout";
+import AdminMenu from "../../Components/Layout/AdminMenu";
 
 function UpdateProduct() {
   return (
+    <Layout>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1>Create Product</h1>
+            <div className="m-1 w-75">
+              <Select
+                outline={"none"}
+                // isSearchable
+                // showSearch
+                // defaultValue={options[0].value}
+                // value={state}
+                // options={options}
+                placeholder="Select a Category"
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                {categories.map((e) => (
+                  <option key={e._id} value={e._id}>
+                    {e.name}
+                  </option>
+                ))}
+              </Select>
+              <div className="mb-3">
+                <label className="btn btn-outline-secondary col-md-12">
+                  {photo ? photo.name : "Upload Photo"}
+                  <input
+                    type="file"
+                    name="photo"
+                    accept="image/*"
+                    onChange={(e) => setPhoto(e.target.files[0])}
+                    hidden
+                  />
+                </label>
+              </div>
+              <div className="mb-3">
+                {photo && (
+                  <div className="text-center">
+                    <img
+                      src={URL.createObjectURL(photo)}
+                      alt="product_photo"
+                      height={"200px"}
+                      className="img img-responsive"
+                    />
+                  </div>
+                )}
+              </div>
 
-      <Layout></Layout>
-   
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={name}
+                  placeholder="write a name"
+                  className="form-control"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <textarea
+                  type="text"
+                  value={description}
+                  placeholder="write a description"
+                  className="form-control"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={price}
+                  placeholder="write a Price"
+                  className="form-control"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={quantity}
+                  placeholder="write a quantity"
+                  className="form-control"
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <Select
+                  // bordered={false}
+                  placeholder="Select Shipping "
+                  size="large"
+                  // showSearch
+                  className="form-select mb-3"
+                  onChange={(value) => {
+                    setShipping(value);
+                  }}
+                >
+                  <option value="0">No</option>
+                  <option value="1">Yes</option>
+                </Select>
+              </div>
+              <div className="mb-3">
+                <Button onClick={handleCreateProduct}>Create Product</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 }
 export default UpdateProduct;
